c3b09a8a84c19142dc30b0c722a61ef7fedaff29
Antonio Isaac Ramirez Monsalve <metal_invaders_21@hotmail.com>
2022-10-09 17:56:28 -0300

    Portafolio

---
+++ src/app/prime-ng.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports:[ButtonModule,CardModule,DividerModule]
+})
+export class PrimeNgModule { }
